2199f24ea8ea68db0714c01790cd6572a2bea92f
Ryan ardiansyah <32880645+ryanardiansyah407@users.noreply.github.com>
2026-05-29 01:20:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 const API_URL =
   "https://script.google.com/macros/s/AKfycbyw5RRj5UcAuZU0bwdKbBo1-iOlXz5JtuaVooo-d_GMPqCxVyQbfpXu7FIkrYtCl-e_/exec";
+
+const CLIENT_ID = "YOUR_CLIENT_ID";
+const DEVELOPER_KEY = "YOUR_API_KEY";
