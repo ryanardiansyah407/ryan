0a3719b914e9d522d851bfbd4a881bd84e99d07e
Ryan ardiansyah <32880645+ryanardiansyah407@users.noreply.github.com>
2026-05-29 01:07:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbw1raVVOBKKjQDfEabYPLwWl8inSr50VgLIPbj0_8EnoiVooVG6WhNjxd4xrfj7ba8y/exec";
+  "https://script.google.com/macros/s/AKfycbyw5RRj5UcAuZU0bwdKbBo1-iOlXz5JtuaVooo-d_GMPqCxVyQbfpXu7FIkrYtCl-e_/exec";
